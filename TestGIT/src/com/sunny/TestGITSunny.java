d6149f11ce3d40733b8b027911dfa324e5bffe99
seeker13 <surrealsunny@gmail.com>
2015-05-31 19:19:22 -0500

    My first java Project checkin

---
+++ TestGIT/src/com/sunny/TestGITSunny.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sunny;
+
+/**
+ * @author Sunny
+ *
+ */
+public class TestGITSunny {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HELLO - TESTING THE GIT HUB USING ECLIPSE");
+
+	}
+
+}
